0f8052ea83b1c3c2a619e3cff45fb14026435eb2
unknown <claygifford@gmail.com>
2023-06-13 10:46:42 -0400

    chore: responsive width changes to login

--- components/auth/log-in-component.tsx
+++ components/auth/log-in-component.tsx
@@ -19,8 +19,8 @@ export default function LogInComponent() {
   }
 
   return (
-    <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="w-[400px] max-w-[400px] space-y-8">
+    <div className="flex w-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-[400px] space-y-8">
         <div>
           <div className="flex justify-center">
             <NoteIcon className="fill-blue-600" />
